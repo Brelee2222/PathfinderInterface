c73da71cd99bcfc61dd89271dfed2bd9c91faa93
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2022-06-01 17:01:56 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,104 @@
+class Pathfinder {
+
+    positions = {};
+
+    maxDistance;
+
+    startPathfinding(startPos, endPos) {
+        this.endPos = endPos;
+        this.maxDistance = NaN;
+        let rootNode = new PathNode(startPos, 0, null, []);
+        let checkNodes = [rootNode];
+
+        while(checkNodes.length) {
+            let checkingNodes = checkNodes;
+            checkNodes = [];
+            for(let node of checkingNodes) {
+                if(this.maxDistance < node.distance)
+                    continue;
+                for(let child of this.getPaths(node)) {
+                    if(!checkingNodes.includes(child))
+                        checkingNodes.push(child);
+                    if(!child.parentNodes.includes(node)) {
+                        for(let path of node.parentLinks) 
+                            if(path.isClearPath(child)) {
+                                path.childLinks.push(child);
+                                child.parentLinks.push(path);
+                            }
+                        child.parentNodes.push(node);
+                    }
+                }
+            }
+        }
+        let endNode = this.positions[endPos];
+        if(!endNode)
+            return;
+        checkNodes = [rootNode];
+        while(checkNodes.length) {
+            let checkingNodes = checkNodes;
+            checkNodes = [];
+            for(let node of checkingNodes) {
+                for(let child of node.childLinks){
+                    let distance = child.distanceTo(node.position) + node.distance;
+                    if(distance <= child.distance) {
+                        child.distance = distance;
+                        child.parentNodes[0] = node;
+                        if(!checkingNodes.includes(child))
+                            checkingNodes.push(child);
+                    }
+                }
+            }
+        }
+        let path = new FollowPath(undefined, endPos);
+        while(endNode.parentNodes[0])
+            path = new FollowPath(path, (endNode = endNode.parentNodes[0]).position);
+        return path;
+    }
+
+    getPaths(node) {
+        return [];
+    }
+}
+
+class PathNode {
+    parentNodes = [];
+    parentLinks = [];
+    childLinks = [];
+    position;
+    distance;
+
+    constructor(position, distance, parentNode, parentLinks) {
+        this.position = position;
+        this.distance = distance;
+        this.parentNodes = [parentNode];
+        for(let parentLink of parentLinks)
+            if(this.isClearPath(parentLink)) {
+                this.parentLinks.push(parentLink);
+                parentLink.childLinks.push(this);
+            }
+        if(!parentLinks.length)
+            if(parentNode) {
+                this.parentLinks.push(parentNode);
+                parentNode.childLinks.push(this)
+            } else
+                this.parentLinks.push(this);
+    }
+
+    isClearPath(path) {
+        return false;
+    }
+
+    distanceTo(position) {
+        return 0;
+    }
+}
+
+class FollowPath {
+    next;
+    position;
+
+    constructor(next, position) {
+        this.next = next;
+        this.position = position;
+    }
+}
